4cd5ee65d2a4516b3f4e8eeefbba1bab2c4ca0e5
AntonioCS <antoniocs@gmail.com>
2018-10-19 08:03:50 +0100

    Trying different approach

---
+++ AcsGE_ECS/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "AcsGE/Game.h"
+#include <iostream>
+#include <utility> //std::pair
+
+#include "MainState.h"
+
+int main(int argc, char *argv[])
+{
+    AcsGameEngine::Game game{"Test", {0, 0}, {1200, 800}};
+
+    game.gsm.addState<MainState>("FirstState");
+
+
+    try {
+        game.Init();
+        game.Run();
+    } catch (std::exception &e) {
+        std::cout << "Exception: " << e.what() << '\n';
+    }
+
+    game.Cleanup();
+
+    return 0;
+}
