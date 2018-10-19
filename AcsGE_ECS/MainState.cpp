789e782bd6c5105880636b222e90e1c659c4e213
AntonioCS <antoniocs@gmail.com>
2018-10-19 08:07:05 +0100

    WIP

---
+++ AcsGE_ECS/MainState.cpp
@@ -0,0 +1,1 @@
+﻿#include "MainState.h"
---
+++ AcsGE_ECS/MainState.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "AcsGE/GameState.h"
+
+class MainState : public AcsGameEngine::GameState
+{
+
+public:
+    using AcsGameEngine::GameState::GameState;
+};
